9783688e736343a4e5b9a450575a809fa16bad37
zhangyufeng <zhangyf3@hyc.cn>
2023-07-21 17:45:53 +0800

    开始实现小功能

---
+++ Sources/XTerminal/Enumerations/SessionModeEnum.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace XTerminal.Enumerations
+{
+    /// <summary>
+    /// 终端模式
+    /// </summary>
+    public enum SessionModeEnum
+    {
+        /// <summary>
+        /// 禁用输入
+        /// </summary>
+        DisbaleInput,
+
+        /// <summary>
+        /// 画图模式，可以在终端上画图
+        /// </summary>
+        Drawing,
+    }
+}
---
+++ Sources/XTerminal/ViewModels/CollectionItem/TextItemVM.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using XTerminal.Document;
+
+namespace XTerminal.ViewModels.CollectionItem
+{
+    public class TextItemVM : CollectionItemVM
+    {
+        public string Text { get; set; }
+
+        public List<VTextAttribute> TextAttributes { get; private set; }
+
+        public TextItemVM()
+        {
+            this.TextAttributes = new List<VTextAttribute>();
+        }
+    }
+}
