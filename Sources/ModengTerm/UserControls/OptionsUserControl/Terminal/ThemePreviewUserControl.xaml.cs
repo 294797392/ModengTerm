03d0c061b9d74e18955c1709fb63d6dbea5d5df8
oheiheiheiheihei <294797392@qq.com>
2024-10-18 14:00:52 +0800

    完善编辑主题界面，增加了一个简单的预览界面

--- Sources/ModengTerm/UserControls/OptionsUserControl/Terminal/ThemePreviewUserControl.xaml.cs
+++ Sources/ModengTerm/UserControls/OptionsUserControl/Terminal/ThemePreviewUserControl.xaml.cs
@@ -1,22 +1,11 @@
-﻿using ModengTerm.Controls;
-using ModengTerm.Document;
+﻿using ModengTerm.Document;
 using ModengTerm.Document.Rendering;
 using ModengTerm.Terminal.DataModels;
 using ModengTerm.Terminal.Parsing;
-using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
 using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 using WPFToolkit.MVVM;
 
 namespace ModengTerm.UserControls.OptionsUserControl.Terminal
@@ -41,8 +30,10 @@ namespace ModengTerm.UserControls.OptionsUserControl.Terminal
         private class ColorItem
         {
             public string Name { get; set; }
-            
+
             public Brush Brush { get; set; }
+
+            public string Character { get; set; }
         }
 
         public ThemePackage Theme
@@ -54,6 +45,68 @@ namespace ModengTerm.UserControls.OptionsUserControl.Terminal
         // Using a DependencyProperty as the backing store for Theme.  This enables animation, styling, binding, etc...
         public static readonly DependencyProperty ThemeProperty =
             DependencyProperty.Register("Theme", typeof(ThemePackage), typeof(ThemePreviewUserControl), new PropertyMetadata(null, ThemePropertyChangedCallback));
+
+
+
+
+        public Color BackgroundColor
+        {
+            get { return (Color)GetValue(BackgroundColorProperty); }
+            set { SetValue(BackgroundColorProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for BackgroundColor.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty BackgroundColorProperty =
+            DependencyProperty.Register("BackgroundColor", typeof(Color), typeof(ThemePreviewUserControl), new PropertyMetadata(Colors.Transparent, BackgroundColorPropertyChangedCallback));
+
+
+
+
+
+        public int PreviewFontSize
+        {
+            get { return (int)GetValue(PreviewFontSizeProperty); }
+            set { SetValue(PreviewFontSizeProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for PreviewFontSize.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty PreviewFontSizeProperty =
+            DependencyProperty.Register("PreviewFontSize", typeof(int), typeof(ThemePreviewUserControl), new PropertyMetadata(12, PreviewFontSizePropertyChangedCallback));
+
+
+
+
+        public string PreviewFontFamily
+        {
+            get { return (string)GetValue(PreviewFontFamilyProperty); }
+            set { SetValue(PreviewFontFamilyProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for PreviewFontFamily.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty PreviewFontFamilyProperty =
+            DependencyProperty.Register("PreviewFontFamily", typeof(string), typeof(ThemePreviewUserControl), new PropertyMetadata(string.Empty, PreviewFontFamilyPropertyChangedCallback));
+
+
+
+
+        public Color PreviewFontColor
+        {
+            get { return (Color)GetValue(PreviewFontColorProperty); }
+            set { SetValue(PreviewFontColorProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for PreviewFontColor.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty PreviewFontColorProperty =
+            DependencyProperty.Register("PreviewFontColor", typeof(Color), typeof(ThemePreviewUserControl), new PropertyMetadata(Colors.Transparent, PreviewFontColorPropertyChangedCallback));
+
+
+
+
+
+
+
+
+
 
 
 
@@ -77,6 +130,8 @@ namespace ModengTerm.UserControls.OptionsUserControl.Terminal
         {
             BindableCollection<ColorItem> colorList = new BindableCollection<ColorItem>();
 
+            char firstChar = 'A';
+
             foreach (VTColorIndex colorIndex in colorIndexs)
             {
                 VTColor vtColor = theme.ColorTable.GetColor(colorIndex);
@@ -86,8 +141,11 @@ namespace ModengTerm.UserControls.OptionsUserControl.Terminal
                 {
                     Name = colorIndex.ToString(),
                     Brush = brush,
+                    Character = firstChar.ToString()
                 };
                 colorList.Add(colorItem);
+
+                firstChar++;
             }
 
             return colorList;
@@ -97,8 +155,8 @@ namespace ModengTerm.UserControls.OptionsUserControl.Terminal
         {
             if (newValue != null) 
             {
-                ListBoxBrightColors.DataContext = this.InitializeColorList(newValue, BrightColors);
-                ListBoxDarkColors.DataContext = this.InitializeColorList(newValue, DarkColors);
+                ListBoxBrightColorsFont.DataContext = ListBoxBrightColors.DataContext = this.InitializeColorList(newValue, BrightColors);
+                ListBoxDarkColorsFont.DataContext = ListBoxDarkColors.DataContext = this.InitializeColorList(newValue, DarkColors);
             }
         }
 
@@ -107,5 +165,69 @@ namespace ModengTerm.UserControls.OptionsUserControl.Terminal
             ThemePreviewUserControl me = d as ThemePreviewUserControl;
             me.OnThemePropertyChanged(e.OldValue as ThemePackage, e.NewValue as ThemePackage);
         }
+
+
+
+
+        private void OnBackgroundColorPropertyChanged(object oldValue, object newValue)
+        {
+            if (newValue != null)
+            {
+                BorderBackground.Background = new SolidColorBrush((Color)newValue);
+            }
+        }
+
+        private static void BackgroundColorPropertyChangedCallback(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            ThemePreviewUserControl me = d as ThemePreviewUserControl;
+            me.OnBackgroundColorPropertyChanged(e.OldValue, e.NewValue);
+        }
+
+
+        private void OnPreviewFontColorPropertyChanged(object oldValue, object newValue)
+        {
+            if (newValue != null)
+            {
+                TextBlockFontPreview.Foreground = new SolidColorBrush((Color)newValue);
+            }
+        }
+
+        private static void PreviewFontColorPropertyChangedCallback(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            ThemePreviewUserControl me = d as ThemePreviewUserControl;
+            me.OnPreviewFontColorPropertyChanged(e.OldValue, e.NewValue);
+        }
+
+
+
+        private void OnPreviewFontSizePropertyChanged(object oldValue, object newValue)
+        {
+            if (newValue != null)
+            {
+                TextBlockFontPreview.FontSize = (int)newValue;
+            }
+        }
+
+        private static void PreviewFontSizePropertyChangedCallback(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            ThemePreviewUserControl me = d as ThemePreviewUserControl;
+            me.OnPreviewFontSizePropertyChanged(e.OldValue, e.NewValue);
+        }
+
+
+        private void OnPreviewFontFamilyPropertyChanged(object oldValue, object newValue)
+        {
+            if (newValue != null)
+            {
+                TextBlockFontPreview.FontFamily = new FontFamily(newValue.ToString());
+            }
+        }
+
+        private static void PreviewFontFamilyPropertyChangedCallback(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            ThemePreviewUserControl me = d as ThemePreviewUserControl;
+            me.OnPreviewFontFamilyPropertyChanged(e.OldValue, e.NewValue);
+        }
+
     }
 }
