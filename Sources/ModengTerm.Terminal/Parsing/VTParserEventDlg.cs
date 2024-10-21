8eaa1605f94a15c4f2e4c353ff0921ba632eccfa
oheiheiheiheihei <294797392@qq.com>
2024-10-21 15:12:05 +0800

    删除无用项目和文件

--- Sources/ModengTerm.Terminal/Parsing/VTParserEventDlg.cs
+++
@@ -1,9 +0,0 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-
-namespace ModengTerm.Terminal.Parsing
-{
-    public delegate void VTParserEventDlg(VTParser parser, VTActions action, object parameter);
-}
