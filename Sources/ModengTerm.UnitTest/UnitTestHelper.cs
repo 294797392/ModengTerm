a509dccfeda0523b7986fbe72c6332ae87bcfbee
oheiheiheiheihei <294797392@qq.com>
2024-11-14 14:33:03 +0800

    新建一个简单的unitTest框架

---
+++ Sources/ModengTerm.UnitTest/UnitTestHelper.cs
@@ -0,0 +1,21 @@
+﻿using ModengTerm.Document;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ModengTerm.UnitTest
+{
+    public static class UnitTestHelper
+    {
+        public static VTDocument CreateVTDocument()
+        {
+            VTDocumentOptions options = new VTDocumentOptions() { };
+
+            VTDocument document = new VTDocument(options);
+            document.Initialize();
+            return document;
+        }
+    }
+}
